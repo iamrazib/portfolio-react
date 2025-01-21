614d28c2a35269a69d1cfb8548c78b5cae5b9908
DESKTOP-8GG43QF\Asus <iamrazib@gmail.com>
2025-01-22 01:03:19 +0600

    footer added

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Footer.css'
+import footer_logo from '../../assets/footer_logo.svg'
+import user_icon from '../../assets/user_icon.svg'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+            <div className="footer-top">
+                <div className="footer-top-left">
+                    <img src={footer_logo} alt="" />
+                    <p>I am a backend developer from Dhaka, Bangladesh with 15+ years of experience in multiple companies like Netweaver Software, Beximco Textiles, Dutch-Bangla Bank, Mutual Trust Bank and Pubali Bank</p>
+                </div>
+            </div>
+            <div className="footer-top-right">
+                <div className="footer-email-input">
+                    <img src={user_icon} alt="" />
+                    <input type="email" placeholder='Enter your email' />
+                </div>
+                <div className="footer-subscribe">Subscribe</div>
+            </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">
+            2025 Razibul, All rights reserved 
+        </p>
+        <div className="footer-bottom-right">
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
