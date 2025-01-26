0d97108aa8935581903522f0ef8b7e250de0d44e
DESKTOP-8GG43QF\Asus <iamrazib@gmail.com>
2025-01-27 01:02:57 +0600

    project image modified

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,40 +1,59 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+
+import project1_dbc_u_dash from '../assets/proj1_dbc_user_dashboard.png'
+import project2_dbc_u_regist from '../assets/proj2_dbc_user_registration.png'
+import project3_dbc_regist_scr from '../assets/proj3_dbc_registration_screen.png'
+import project4_erecruitment from '../assets/proj4_e-recruitment.png'
+import project5_pblweb from '../assets/proj5_pblweb.png'
+import project6_mtb_remit_system from '../assets/proj6_mtb_remit_system.png'
+import project7_dbbl_remit from '../assets/proj7_dbbl_remittance.png'
+import project8_dbbl_Rocket from '../assets/proj8_dbbl_Rocket.png'
+import project9_sensiblematch from '../assets/proj9_sensiblematch.png'
 
 const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:project1_dbc_u_dash
     },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:project2_dbc_u_regist
     },
     {
         w_no:3,
         w_name:"Web design",
-        w_img:project3_img
+        w_img:project3_dbc_regist_scr
     },
     {
         w_no:4,
         w_name:"Web design",
-        w_img:project4_img
+        w_img:project4_erecruitment
     },
     {
         w_no:5,
         w_name:"Web design",
-        w_img:project5_img
+        w_img:project5_pblweb
     },
     {
         w_no:6,
         w_name:"Web design",
-        w_img:project6_img
+        w_img:project6_mtb_remit_system
+    },
+    {
+        w_no:7,
+        w_name:"Web design",
+        w_img:project7_dbbl_remit
+    },
+    {
+        w_no:8,
+        w_name:"Web design",
+        w_img:project8_dbbl_Rocket
+    },
+    {
+        w_no:9,
+        w_name:"Web design",
+        w_img:project9_sensiblematch
     },
 ]
  
